8a31a2985b613f0ddba2aee51dfcc1c85c110579
partywagon112 <patrick.curtain.112@outlook.com>
2023-07-30 20:14:56 +1000

    Updated readme.

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Spring Pin
+A few KiCad plugins meant to help with automating tasks required for spring pins.
+
+The best way to automate assembly for PCBs where spring pins are used where connectors normally are is to add in individual items. 
+This can be tedious, so some automation is necessary. 
+
+# Installation (Kicad 6.0 >)
+Run setup.bat for Windows.
+
+Linux assume you know what you're doing, cp ./src/SpringPin into the plugins directory.
+
+# Future
+Probably should make something that can interact with the schematic to place the required items... but too hard! 
+C'mon KiCad provide a Python API for EESchema.
--- setup.bat
+++ setup.bat
@@ -5,8 +5,15 @@ call "C:\Program Files\KiCad\7.0\bin\kicad-cmd.bat"
 cd %mypath%
 call pip install --editable .
 
-@REM robocopy .\src\ C:\Users\%username%\OneDrive\Documents\KiCad\7.0\scripting\plugins /E
-robocopy .\src\ %APPDATA%/Roaming/kicad/scripting/plugins /E
+if exist "C:\Users\%username%\OneDrive\Documents\KiCad\7.0\scripting\plugins" (
+    robocopy .\src\ C:\Users\%username%\OneDrive\Documents\KiCad\7.0\scripting\plugins /E
+) else (
+    if exist "C:\Users\%username%\Documents\KiCad\7.0\scripting\plugins" (
+        robocopy .\src\ C:\Users\%username%\Documents\KiCad\7.0\scripting\plugins /E
+    ) else (
+        echo No directory found to install plugins! Copy and Paste ./src/SpringPin manually into the KiCad plugins directory.
+    )
+)
 
 echo Done
 pause
